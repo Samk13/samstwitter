8a1697719380b778065b40f6e83b7d927d6debc1
Samk13 <samarbid13@gmail.com>
2020-10-25 14:00:56 +0100

    laravel init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+        // defaultLineHeights: true,
+        // standardFontWeights: true
+    },
+    purge: [],
+    theme: {
+        fontFamily: {
+            sans: ["Roboto", "Helvetica", "Arial", "sans-serif"],
+            serif: ["'Alfa Slab One'", "cursive"]
+        },
+        extend: {
+            // or fontFamily should be here I am not sure, but it works for me
+        }
+    },
+    variants: {},
+    plugins: []
+};
